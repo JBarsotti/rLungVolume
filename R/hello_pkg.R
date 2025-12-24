02dc02d5962df7d6e5c66a5d554920ed1a13fbf2
John Barsotti <ebarsot11@gmail.com>
2025-12-23 17:55:04 -0700

    Added first R Function

---
+++ R/hello_pkg.R
@@ -0,0 +1,11 @@
+#' Say hello from rLungVolume
+#'
+#' A tiny utility that returns a friendly greeting from the package.
+#'
+#' @return A character scalar greeting.
+#' @examples
+#' hello_pkg()
+#' @export
+hello_pkg <- function() {
+  "Hello from rLungVolume!"
+}
